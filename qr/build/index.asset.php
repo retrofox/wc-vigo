947e66202ffff7c28bc5a1dda0b59454c047f937
Damián Suárez <rdsuarez@gmail.com>
2024-05-22 14:38:15 +0100

    not revision builds

--- qr/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e20a18a6e860db7283a2');
--- qr/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '05460330032eb63c4793');
