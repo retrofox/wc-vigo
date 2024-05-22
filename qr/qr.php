d8260f05e13de19df7463fd520c0fe2e449a995d
Damián Suárez <rdsuarez@gmail.com>
2024-05-22 13:41:43 +0100

    update title and description

--- qr/qr.php
+++ qr/qr.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       QR block
- * Description:       QR block
+ * Description:       QR block utilities
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Damián Suárez (retrofox)
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       qr
