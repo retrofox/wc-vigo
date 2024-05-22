22f7f4e9da353721d65fb28828e59400952e17e9
Damián Suárez <rdsuarez@gmail.com>
2024-05-22 17:10:37 +0100

    edit content attribute

--- qr/src/edit.js
+++ qr/src/edit.js
@@ -2,12 +2,14 @@
  * External dependencies
  */
 import { QRCodeSVG } from 'qrcode.react';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
  */
 import './editor.scss';
+import { Panel, PanelBody, TextControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Edit function for the QR block.
@@ -16,11 +18,28 @@ import './editor.scss';
  * @return {Element} Element to render.
  */
 export default function QRBlockEdit( props ) {
-	const { attributes } = props;
+	const { attributes, setAttributes } = props;
 
 	return (
 		<div { ...useBlockProps() }>
 			<div { ...useBlockProps() }>
+				<InspectorControls>
+					<Panel>
+						<PanelBody title="QR Code Settings">
+							<TextControl
+								label={ __( 'Content' ) }
+								value={ attributes?.content }
+								onChange={ ( value ) =>
+									setAttributes( { content: value } )
+								}
+								help={ __(
+									'The content to encode in the QR code.'
+								) }
+							/>
+						</PanelBody>
+					</Panel>
+				</InspectorControls>
+
 				<QRCodeSVG value={ attributes?.content } />
 				<p className="qr__content">{ attributes?.content }</p>
 			</div>
