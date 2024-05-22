293cdaf5ef791ea437aa03630578a194aedce477
Damián Suárez <rdsuarez@gmail.com>
2024-05-22 15:37:40 +0100

    save QR block

--- qr/src/save.js
+++ qr/src/save.js
@@ -1,24 +1,10 @@
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
+import QRCode from 'qrcode.react';
 import { useBlockProps } from '@wordpress/block-editor';
 
-/**
- * The save function defines the way in which the different attributes should
- * be combined into the final markup, which is then serialized by the block
- * editor into `post_content`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#save
- *
- * @return {Element} Element to render.
- */
 export default function save() {
 	return (
-		<p { ...useBlockProps.save() }>
-			{ 'QR block – hello from the saved content!' }
-		</p>
+		<figure { ...useBlockProps.save() }>
+			<QRCode value="Hola" renderAs="svg" />
+		</figure>
 	);
 }
